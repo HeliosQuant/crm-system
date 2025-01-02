4f69c722b1382c964a48fc4fb960318036b1eb23
Helios Quant <heliosquant@gmail.com>
2025-01-02 00:52:57 -0800

    create template for views

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
 
 def home_page(request):
-    return HttpResponse("Hello world")
+    #return render(request, "leads/home_page.html") # that's an option
+    context = {
+        'name': 'Joe',
+        'age': 30
+    }
+    return render(request, 'second_page.html', context)
